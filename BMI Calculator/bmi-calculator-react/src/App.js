72bf8250366c127d46e80b8c613c334bb3875395
ahleem <72522827+AhleemAkinwande@users.noreply.github.com>
2023-02-08 23:03:05 -0500

    Completed BMI Calculator
    Added a function to calculate the bmi
    added logic to prompt user to input the correct value in for height, weight, or both
    added logic to present a message via the useState and if else/else ifs
    created an assets folder to hold images
    added logic to update images and display them on the page using the images in the assets folder

--- BMI Calculator/bmi-calculator-react/src/App.js
+++ BMI Calculator/bmi-calculator-react/src/App.js
@@ -11,24 +11,70 @@ function App() {
   const [bmi, setBmi] = useState("");
   const [message, setMessage] = useState("");
 
-  let imgSrc = ""
+  let bmiCalculation = (e) => {
+    // prevent submitting
+    e.preventDefault();
+
+    if (isNaN(height) || height === 0) {
+      if (isNaN(weight) || weight === 0) {
+        alert("Please enter a valid weight or height");
+    } else {
+      alert("Please enter a valid height");
+      }
+     } 
+    else if (isNaN(weight) || weight === 0) {
+      alert("Please enter a valid weight");
+    } else {
+      let bmi = ((weight / (height ** 2)) * 703);
+    setBmi(bmi.toFixed(1)); // toFixed(1) sets the number of decimal digits
+
+    // Logic for message
+    if (bmi < 25) {
+      setMessage("You are underweight")
+    } else if (bmi >= 25 && bmi < 30) {
+      setMessage("You are a healthy weight")
+    } else {
+      setMessage("You are overweight")
+    }
+  }
+}
+
+  // Show image based on BMI calculation
+  let imgSrc = "";
+
+  if (bmi < 1) {
+    imgSrc = null;
+  } else {
+    if (bmi < 25) {
+    imgSrc = require("../src/assets/underweight.png")
+  } else if (bmi >= 25 && bmi < 30) {
+    imgSrc = require("../src/assets/healthy.png")
+  } else {
+    imgSrc = require("../src/assets/overweight.png")
+  }
+};
+
+
+let reload = () => {
+  window.location.reload();
+};
 
   return (
     <div className="app">
       <div className="container">
         <h2 className="center">BMI Calculator</h2>
-        <form>
+        <form onSubmit={bmiCalculation}>
           <div>
             <label>Weight (lbs)</label>
-            <input value={weight} />
+            <input value={weight} onChange={(e) => setWeight(e.target.value)} />
           </div>
           <div>
             <label>Height (in)</label>
-            <input value={height} />
+            <input value={height} onChange={(e) => setHeight(e.target.value)} />
           </div>
           <div>
             <button className="btn" type="submit">Submit</button>
-            <button className="btn btn-outline" type="submit">Submit</button>
+            <button className="btn btn-outline" type="submit" onClick={reload}>Reload</button>
           </div>
         </form>
 
@@ -37,7 +83,7 @@ function App() {
           <p>{message}</p>
         </div>
         
-        <div className="image-container">
+        <div className="img-container">
           <img src={imgSrc} alt=""></img>
         </div>
       </div>
